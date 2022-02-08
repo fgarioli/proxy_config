5890770f0a73d246610b2bcddadfeccad77b0fd5
Fernando Garioli <fernando.garioli@ifes.edu.br>
2022-02-07 21:56:10 -0300

    adicao de referencias

--- cntlm.bat
+++ cntlm.bat
@@ -1,3 +1,9 @@
 "C:\Program Files (x86)\Cntlm\cntlm.exe" -H
 set HTTP_PROXY=http://172.16.48.1:3128
 set HTTPS_PROXY=http://172.16.48.1:3128
+
+https://smalldata.tech/blog/2018/09/26/local-proxy-using-cntlm
+https://stackoverflow.com/questions/9181637/how-to-fill-in-proxy-information-in-cntlm-config-file
+http://softwarelivre.org/gutocarvalho/blog/cntlm-uma-solucao-rapida-e-elegante?lang=es
+https://gist.github.com/goude/edecda33699fcad5d66e
+https://gist.github.com/erkobridee/4061363
